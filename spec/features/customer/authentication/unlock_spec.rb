57ab82ae504ba259ea4f4b8416cb67ef43e1edc7
Bryce Senz <bryce.senz@gmail.com>
2013-02-17 13:31:22 +0900

    All customer feature specs passing

--- spec/features/customer/authentication/unlock_spec.rb
+++ spec/features/customer/authentication/unlock_spec.rb
@@ -1,47 +1,62 @@
 require 'spec_helper'
 
 describe "unlock account" do
-  context "anonymous", :anonymous => true do
-    include_context "as anonymous visitor"
+  context "as anonymous", :anonymous => true do
+    include_context "as anonymous"
     let(:customer) { FactoryGirl.create(:customer) }
     before(:each) do
       visit new_customer_unlock_path
     end
     
     context "unlocked" do
+      before(:each) do
+        customer.unlock_access!
+        reset_email
+      end
       describe "with valid attributes" do
-        it "does not send unlock email" do
-          #TODO: Find a better way to test this
-        end
-  
         it "reloads unlock page" do
-          within("#customer-unlock") do
+          within("#unlock") do
             fill_in 'customer_email', :with => customer.email
-            click_button 'Resend unlock instructions'
+            click_button 'Send Unlock Instructions'
           end
           
+          # App Behavior
           current_path.should eq(customer_unlock_path)
           page.should have_css("#flash-messages")
           message = YAML.load_file("#{Rails.root}/config/locales/devise.en.yml")['en']['errors']['messages']['not_locked']
-          page.should have_content(message)          
+          page.should have_content(message)
+        end      
+
+        it "does not send email" do
+          within("#unlock") do
+            fill_in 'customer_email', :with => customer.email
+            click_button 'Send Unlock Instructions'
+          end
+          
+          last_email.should be_nil
         end      
       end
   
       describe "with invalid email" do
-        it "does not send unlock email" do
-          #TODO: Find a better way to test this
-        end      
-  
         it "reloads unlock page" do
-          within("#customer-unlock") do
+          within("#unlock") do
             fill_in 'customer_email', :with => "invalid@email.com"
-            click_button 'Resend unlock instructions'
+            click_button 'Send Unlock Instructions'
           end
           
           current_path.should eq(customer_unlock_path)
           page.should have_css("#flash-messages")
           message = "not found"
           page.should have_content(message)          
+        end      
+
+        it "does not send email" do
+          within("#unlock") do
+            fill_in 'customer_email', :with => customer.email
+            click_button 'Send Unlock Instructions'
+          end
+          
+          last_email.should be_nil
         end      
       end
     end
@@ -49,26 +64,14 @@ describe "unlock account" do
     context "locked" do
       before(:each) do
         customer.lock_access!
+        reset_email
       end
 
       describe "with valid attributes" do
-        it "sends unlock reset email" do
-          within("#customer-unlock") do
-            fill_in 'customer_email', :with => customer.email
-            click_button 'Resend unlock instructions'
-          end
-          
-          reset_email = ActionMailer::Base.deliveries.last
-          reset_email.to.should eq([customer.email])
-          reset_email.from.should eq(["no-reply@credda.com"])
-          subject = YAML.load_file("#{Rails.root}/config/locales/devise.en.yml")['en']['devise']['mailer']['unlock_instructions']['subject']
-          reset_email.subject.should eq(subject)
-        end      
-  
         it "redirects to sign in page" do
-          within("#customer-unlock") do
+          within("#unlock") do
             fill_in 'customer_email', :with => customer.email
-            click_button 'Resend unlock instructions'
+            click_button 'Send Unlock Instructions'
           end
           
           current_path.should eq(new_customer_session_path)
@@ -76,13 +79,25 @@ describe "unlock account" do
           message = YAML.load_file("#{Rails.root}/config/locales/devise.en.yml")['en']['devise']['unlocks']['send_instructions']
           page.should have_content(message)          
         end      
+
+        it "sends unlock reset email" do
+          within("#unlock") do
+            fill_in 'customer_email', :with => customer.email
+            click_button 'Send Unlock Instructions'
+          end
+          
+          last_email.to.should eq([customer.email])
+          last_email.from.should eq(["no-reply@credda.com"])
+          subject = YAML.load_file("#{Rails.root}/config/locales/devise.en.yml")['en']['devise']['mailer']['unlock_instructions']['subject']
+          last_email.subject.should eq(subject)
+        end      
       end
   
       describe "with invalid email" do
         it "reloads unlock page" do
-          within("#customer-unlock") do
+          within("#unlock") do
             fill_in 'customer_email', :with => "invalid@email.com"
-            click_button 'Resend unlock instructions'
+            click_button 'Send Unlock Instructions'
           end
           
           current_path.should eq(customer_unlock_path)
@@ -90,12 +105,22 @@ describe "unlock account" do
           message = "not found"
           page.should have_content(message)          
         end      
+
+
+        it "does not send email" do
+          within("#unlock") do
+            fill_in 'customer_email', :with => "invalid@email.com"
+            click_button 'Send Unlock Instructions'
+          end
+          
+          last_email.should be_nil
+        end      
       end
     end    
   end
   
-  context "authenticated" do
-    include_context "as authenticated customer"
+  context "as customer" do
+    include_context "as customer"
 
     it "redirects to customer home" do
       visit new_customer_unlock_path
